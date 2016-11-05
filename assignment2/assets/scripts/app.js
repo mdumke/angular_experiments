c13c971ceb541c9510aace2775dbebf5643053f4
Matthias Dumke <matthias.dumke@gmx.net>
2016-11-05 16:40:59 +0100

    finish first version of assignment 2

--- assignment2/assets/scripts/app.js
+++ assignment2/assets/scripts/app.js
@@ -1,17 +1,57 @@
 (function () {
 
-  'use strict';
+'use strict';
 
-  angular
-    .module('ShoppingListCheckOff', [])
-    .controller('ToBuyController', ToBuyController);
+angular
+  .module('ShoppingListCheckOff', [])
+  .controller('ToBuyController', ToBuyController)
+  .controller('AlreadyBoughtController', AlreadyBoughtController)
+  .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
-  function ToBuyController () {
-    var toBuyCtrl = this;
+// Define ToBuyController
+ToBuyController.$inject = ['$scope', 'ShoppingListCheckOffService'];
 
-    toBuyCtrl.items = [
-      '10 apples', '1 banana', '5 T-shirts',
-      '2 trips to Alaska', '7 socks'];
-  }
+function ToBuyController ($scope, ShoppingListCheckOffService) {
+  $scope.items = ShoppingListCheckOffService.getItemsToBuy();
+
+  $scope.checkOffItem = function (itemID) {
+    ShoppingListCheckOffService.checkOffItem(itemID);
+  };
+};
+
+// Define AlreadyBougthController
+AlreadyBoughtController.$inject = ['$scope', 'ShoppingListCheckOffService'];
+
+function AlreadyBoughtController ($scope, ShoppingListCheckOffService) {
+  $scope.items = ShoppingListCheckOffService.getItemsBought();
+};
+
+// Define ShoppingListCheckOffService
+function ShoppingListCheckOffService () {
+  var service = this;
+
+  service.itemsToBuy = [
+    { name: 'clones of myself', quantity: 2 },
+    { name: 'competing firms', quantity: 4 },
+    { name: 'spaceship', quantity: 1 },
+    { name: 'socks', quantity: 3 },
+    { name: 'christmas trees', quantity: 8}
+  ];
+
+  service.itemsBought = [];
+
+  service.getItemsToBuy = function () {
+    return service.itemsToBuy;
+  };
+
+  service.getItemsBought = function () {
+    return service.itemsBought;
+  };
+
+  service.checkOffItem = function (itemID) {
+    service.itemsBought.push(service.itemsToBuy[itemID]);
+    service.itemsToBuy.splice(itemID, 1);
+  };
+};
 
 })();
