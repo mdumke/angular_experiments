9a41b1468df7881eb64a21d73c130e4faf2d1ec2
Matthias Dumke <matthias.dumke@gmx.net>
2016-12-18 11:16:00 +0100

    get categories-data from the server

--- assignment4/assets/scripts/src/modules/data/menudata.service.js
+++ assignment4/assets/scripts/src/modules/data/menudata.service.js
@@ -3,32 +3,24 @@
 
 angular
   .module('data')
-  .service('MenuDataService', MenuDataService);
+  .service('MenuDataService', MenuDataService)
+  .constant('ApiBasePath', 'https://davids-restaurant.herokuapp.com');
 
-MenuDataService.$inject = ['$q', '$timeout'];
 
-function MenuDataService ($q, $timeout) {
+MenuDataService.$inject = ['$http', 'ApiBasePath', '$q', '$timeout'];
+
+function MenuDataService ($http, ApiBasePath, $q, $timeout) {
   var service = this;
 
   /* returns a promise to retrieve the categories-json from the server */
   service.getAllCategories = function () {
-    var data = [
-      {
-        "id":81, "short_name":"L", "name":"Lunch",
-        "special_instructions":"Sunday-Friday 11:15am-3:00pm. Served with your choice of rice (Vegetable Fried RIce, Steamed Rice, Brown Rice), AND EITHER soup (Hot \u0026 Sour, Wonton, Vegetable, Egg Drop, Chicken Corn Soup) OR veggie egg roll. $1.00 extra to have both soup and egg roll.","url":"https://davids-restaurant.herokuapp.com/categories/81.json"
-      },{
-        "id":82,"short_name":"A","name":"Soup","special_instructions":"",
-        "url":"https://davids-restaurant.herokuapp.com/categories/82.json"
-      }
-    ]
-
-    var deferred = $q.defer();
-
-    $timeout(function () {
-      deferred.resolve(data);
-    }, 300);
-
-    return deferred.promise;
+    return $http({
+      method: 'GET',
+      url: (ApiBasePath + '/categories.json')
+    })
+    .then(function (result) {
+      return result.data;
+    });
   };
 
   /* returns a promise to retrieve json for the specified category */
